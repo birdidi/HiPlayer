5fae9a5be44444749091e91cd1930b6d7dfdfff2
chenxuyu <chenxuyu@felink.com>
2018-08-03 13:43:46 +0800

    动画矩阵计算优化

--- app/src/main/java/com/android/hi/hiplayer/activity/GuideActivity.java
+++ app/src/main/java/com/android/hi/hiplayer/activity/GuideActivity.java
@@ -2,10 +2,13 @@ package com.android.hi.hiplayer.activity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.animation.ValueAnimator;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -21,11 +24,30 @@ public class GuideActivity extends AppCompatActivity implements View.OnClickList
 
     private static final String TAG = "GuideActivity";
 
+    private static final int WHAT_ANIMATOR_LOOP = 1;
+
     private ImageView ivGallery;
     private View btnGuideIgnore;
     private View btnGuideGo;
     private View btnGuidePlay;
     private int screenW, screenH;
+
+    private Matrix matrixA, matrixB, matrixC;
+    private float[] deltaA = new float[2], deltaB = new float[2], deltaC = new float[2];
+    private float[] scaleA = new float[2], scaleB = new float[2], scaleC = new float[2];
+    private AnimatorSet animatorSet;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == WHAT_ANIMATOR_LOOP) {
+                if (animatorSet != null) {
+                    animatorSet.start();
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,122 +67,171 @@ public class GuideActivity extends AppCompatActivity implements View.OnClickList
         btnGuideIgnore.setOnClickListener(this);
         btnGuideGo.setOnClickListener(this);
         btnGuidePlay.setOnClickListener(this);
+
+        initMatrix();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        performPost1();
-    }
+    private void initMatrix() {
+        int[] drawableWH;
+        float dx, dy;
 
-    private void performPost1() {
-        int[] post1WH = getBitmapWH(R.drawable.ic_guide_1);
-        ivGallery.setImageResource(R.drawable.ic_guide_1);
-        final Matrix matrix = new Matrix();
+        //图一
+        drawableWH = getBitmapWH(R.drawable.ic_guide_1);
+        matrixA = new Matrix();
 
-        float scaleX = (screenW * 0.7f) / post1WH[0];
-        float scaleY = (screenH * 0.7f) / post1WH[1];
+        float scaleX = (screenW * 1.4f) / drawableWH[0];
+        float scaleY = (screenH * 1.4f) / drawableWH[1];
         scaleX = scaleY = Math.max(scaleX, scaleY);
-        Log.d(TAG, "scale: " + scaleX + ", " + scaleY);
-        Log.d(TAG, "screen : " + screenW + ", " + screenH);
-        Log.d(TAG, "image: " + post1WH[0] + ", " + post1WH[1]);
-        final float dx = -(post1WH[0] * scaleX) * (19 / 32.0f);
-        final float dy = -(post1WH[1] * scaleY) * (14 / 32.0f);
-        matrix.setScale(scaleX, scaleY);
-        //matrix.postTranslate(dx * 1.5f, dy * 0f);
-        ivGallery.setImageMatrix(matrix);
+        Log.e(TAG, "scaleA : " + scaleX + ", imageA: " + drawableWH[0] + ", " + drawableWH[1]);
+        dx = -(drawableWH[0] * scaleX) * (10 / 32.0f);
+        dy = -(drawableWH[1] * scaleY) * (7 / 32.0f);
+        scaleA[0] = scaleX;
+        scaleA[1] = scaleY;
+        deltaA[0] = dx;
+        deltaA[1] = dy;
+        matrixA.setScale(scaleX, scaleY);
+//        matrixA.postTranslate(dx, dy);
 
-        ValueAnimator animator = ValueAnimator.ofFloat(0.5f, 1f);
-        animator.setDuration(2000);
-        animator.setInterpolator(new AccelerateDecelerateInterpolator());
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        //图二
+        drawableWH = getBitmapWH(R.drawable.ic_guide_3);
+        scaleX = ((screenW * 1.0f) / drawableWH[0]);
+        scaleY = ((screenH * 1.0f) / drawableWH[1]);
+        scaleX = scaleY = Math.max(scaleX, scaleY);
+        Log.e(TAG, "scaleB : " + scaleX + ", imageB: " + drawableWH[0] + ", " + drawableWH[1]);
+        matrixB = new Matrix();
+        matrixB.setScale(scaleX, scaleY);
+        matrixB.postTranslate((screenW - drawableWH[0] * scaleX) / 2, (screenH - drawableWH[1] * scaleY) / 2);
+
+        scaleB[0] = scaleX;
+        scaleB[1] = scaleY;
+        deltaB[0] = drawableWH[0];
+        deltaB[1] = drawableWH[1];
+
+//        ivGallery.setImageMatrix(matrixB);
+
+        //图三
+        drawableWH = getBitmapWH(R.drawable.ic_guide_3);
+        scaleX = ((screenW * 1.2f) / drawableWH[0]);
+        scaleY = ((screenH * 1.2f) / drawableWH[1]);
+        scaleX = scaleY = Math.max(scaleX, scaleY);
+        Log.e(TAG, "scaleC: " + scaleX + ", imageC: " + drawableWH[0] + ", " + drawableWH[1]);
+        dx = -(drawableWH[0] * scaleX * (1 / 32.0f));
+        dy = -(drawableWH[1] * scaleY * (3 / 32.0f));
+
+        matrixC = new Matrix();
+        matrixC.setScale(scaleX, scaleY);
+//        matrixC.postTranslate(dx, dy);
+
+        scaleC[0] = scaleX;
+        scaleC[1] = scaleY;
+        deltaC[0] = dx;
+        deltaC[1] = dy;
+
+        animatorSet = new AnimatorSet();
+
+        ValueAnimator animatorA = ValueAnimator.ofFloat(0.5f, 1f);
+        animatorA.setDuration(2000);
+        animatorA.setInterpolator(new AccelerateDecelerateInterpolator());
+        animatorA.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 float val = (float) animation.getAnimatedValue();
-                Matrix mm = new Matrix(matrix);
-                mm.postTranslate(dx * (2.0f - val)/* - val * screenW*/, dy * (val - 0.5f) * 2/* - val * screenH*/);
+                Matrix mm = new Matrix(matrixA);
+                mm.postTranslate(deltaA[0] * (2.0f - val), deltaA[1] * (val - 0.5f) * 2);
                 ivGallery.setImageMatrix(mm);
             }
         });
-        animator.addListener(new AnimatorListenerAdapter() {
+        animatorA.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                ivGallery.setImageResource(R.drawable.ic_guide_1);
+            }
+        });
+
+        ValueAnimator animatorB = ValueAnimator.ofFloat(scaleB[0], scaleB[0] + 1.5f);
+        animatorB.setDuration(2000);
+        animatorB.setInterpolator(new AccelerateDecelerateInterpolator());
+        animatorB.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float val = (float) animation.getAnimatedValue();
+                Matrix mm = new Matrix();
+                mm.setScale(val, val);
+                mm.postTranslate((screenW - deltaB[0] * val) / 2 * (29 / 32f), (screenH - deltaB[1] * val) / 2 * (26 / 32f));
+                ivGallery.setImageMatrix(mm);
+            }
+        });
+        animatorB.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                ivGallery.setImageResource(R.drawable.ic_guide_3);
+            }
+        });
+
+        ValueAnimator animatorC = ValueAnimator.ofFloat(4.0f, 1f);
+        animatorC.setDuration(2000);
+        animatorC.setInterpolator(new AccelerateDecelerateInterpolator());
+        animatorC.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float val = (float) animation.getAnimatedValue();
+                Matrix mm = new Matrix(matrixC);
+                mm.postTranslate(deltaC[0] * ((val / 2) + 0.5f), deltaC[1] * val);
+                ivGallery.setImageMatrix(mm);
+            }
+        });
+        animatorC.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                ivGallery.setImageResource(R.drawable.ic_guide_3);
+            }
+        });
+
+        animatorSet.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                performPost2();
+                super.onAnimationEnd(animation);
+                handler.obtainMessage(WHAT_ANIMATOR_LOOP).sendToTarget();
             }
         });
-        animator.start();
-    }
-
-    private void performPost2() {
-        final int[] drawableWH = getBitmapWH(R.drawable.ic_guide_3);
-        ivGallery.setImageResource(R.drawable.ic_guide_3);
-        float scaleX = ((screenW * 0.7f) / drawableWH[0]);
-        float scaleY = ((screenH * 0.7f) / drawableWH[1]);
-        scaleX = scaleY = Math.max(scaleX, scaleY);
-        final Matrix matrix = new Matrix();
-        matrix.setScale(scaleX, scaleY);
-        matrix.postTranslate((drawableWH[0] * scaleX - screenW), (drawableWH[1] * scaleY - screenH));
-        ivGallery.setImageMatrix(matrix);
-
-
-        ValueAnimator animator = ValueAnimator.ofFloat(1.0f, 3.0f);
-        animator.setDuration(2000);
-        animator.setInterpolator(new AccelerateDecelerateInterpolator());
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float val = (float) animation.getAnimatedValue();
-                Matrix mm = new Matrix(matrix);
-                mm.postScale(val, val, drawableWH[0] * 3.0f / 2, drawableWH[1] * 3.0f / 2);
-                mm.postTranslate((drawableWH[0] * val - screenW), (drawableWH[1] * val - screenH));
-                ivGallery.setImageMatrix(mm);
-            }
-        });
-        animator.addListener(new AnimatorListenerAdapter() {
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                performPost3();
-            }
-        });
-        animator.start();
-    }
-
-    private void performPost3() {
-        final int[] drawableWH = getBitmapWH(R.drawable.ic_guide_3);
-        Log.e(TAG, "drawable: " + drawableWH[0] + ", " + drawableWH[1]);
-        ivGallery.setImageResource(R.drawable.ic_guide_3);
-        float scaleX = ((screenW * 0.7f) / drawableWH[0]);
-        float scaleY = ((screenH * 0.7f) / drawableWH[1]);
-        scaleX = scaleY = Math.max(scaleX, scaleY);
-        Log.e(TAG, "scale: " + scaleX);
-        final float dx = -(drawableWH[0] * scaleX * (2 / 32.0f));
-        final float dy = -(drawableWH[1] * scaleY * (7 / 32.0f));
-
-        final Matrix matrix = new Matrix();
-        matrix.setScale(scaleX, scaleY);
-        ivGallery.setImageMatrix(matrix);
-
-        ValueAnimator animator = ValueAnimator.ofFloat(4.0f, 1f);
-        animator.setDuration(2000);
-        animator.setInterpolator(new AccelerateDecelerateInterpolator());
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float val = (float) animation.getAnimatedValue();
-                Matrix mm = new Matrix(matrix);
-                mm.postTranslate(dx * ((val / 2) + 0.5f), dy * val);
-                ivGallery.setImageMatrix(mm);
-            }
-        });
-        animator.start();
+        animatorSet.playSequentially(animatorA, animatorB, animatorC);
     }
 
     private int[] getBitmapWH(int resId) {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeResource(getResources(), resId, options);
-        return new int[]{options.outWidth, options.outHeight};
+        float scale = (ScreenUtil.getDensity(getApplicationContext())) / 1.5f;//HDPI
+        Log.e(TAG, "image scale : " + scale);
+        return new int[]{(int) (options.outWidth * scale), (int) (options.outHeight * scale)};
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (animatorSet != null) {
+            animatorSet.start();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (animatorSet != null) {
+            animatorSet.cancel();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (animatorSet != null) {
+            animatorSet.cancel();
+        }
+        animatorSet = null;
+        super.onDestroy();
     }
 
     @Override
